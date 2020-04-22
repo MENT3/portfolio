c812b253c1d6c174c33d486148306f89451f9591
Clément <clement.rms@gmail.com>
2020-04-22 12:25:47 +0200

    Fix: Active navigation bar

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,21 +1,54 @@
+let observer = null
+const sectionsSpies = document.querySelectorAll('[data-spy]')
+
+const activate = elem => {
+    const id = elem.getAttribute('id')
+    const anchor = document.querySelector(`a[href="#${id}"]`)
+
+    if(anchor == null) return null
+
+    anchor.parentElement.parentElement
+        .querySelectorAll('.active')
+        .forEach(node => node.classList.remove('active'))
+
+    anchor.classList.add('active')
+}
+
+const callback = (entries, observer) => {
+    entries.forEach(entry => { if (entry.intersectionRatio > 0 ) activate(entry.target) })
+}
+
+const observe = elems => {
+    if(observer !== null) elems.forEach(elem => observer.unobserve(elem))
+
+    const y = Math.round(window.innerHeight * .6)
+    observer = new IntersectionObserver(callback, {
+        rootMargin: `-${window.innerHeight - y - 1}px 0px -${y}px 0px`
+    })
+
+    sectionsSpies.forEach(elem => observer.observe(elem))
+}
+
+if (sectionsSpies.length > 0) {
+    observe(sectionsSpies)
+    let windowH = window.innerHeight
+    window.addEventListener('resize', () => {
+        if(window.innerHeight !== windowH) {
+            observe(sectionsSpies)
+            windowH = window.innerHeight
+        }
+    })
+}
+
+
+// CHANGE THIS !!!! 
 jQuery(($) => {
 
     const sections = $('.section')
-    const linkScroll = $('.nav__link')
-    const downArrow = $('.arrow-scroll-down')
-    const contactButton = $("a[href$='#contact']")
 
     $(window).scroll((event) => {
 
         let scrollDistance = $(window).scrollTop()
-
-        sections.each((i) => {
-
-            if (sections.eq(i).position().top <= scrollDistance+150) {
-                $('.nav a.active').removeClass('active');
-                $('.nav .nav__link').eq(i).addClass('active');
-            }
-        })
 
         if($(this).scrollTop() > 10) {
             $('.nav__link, .nav').addClass("fixed")
@@ -25,24 +58,4 @@ jQuery(($) => {
 
 
     })
-
-    linkScroll.on('click', function(e) {
-        e.preventDefault();
-        $('body, html').animate({
-            scrollTop: $(this.hash).offset().top - ($(window).width() <= 756 ? 50 : 100)
-        }, 300);
-    });
-
-    downArrow.on('click', function(e) {
-        $('body, html').animate({
-            scrollTop: $('#about').offset().top - ($(window).width() <= 756 ? 50 : 100)
-        }, 300);
-    })
-
-    contactButton.on('click', function(e) {
-        $('body, html').animate({
-            scrollTop: $('#contact').offset().top - ($(window).width() <= 756 ? 50 : 100)
-        }, 300);
-    })
-
 })
